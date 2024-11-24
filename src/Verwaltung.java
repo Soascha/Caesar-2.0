8ae518fc2895b055146cc44a836993b050f8ea00
Soascha <kosych.sascha@gmail.com>
2024-11-24 19:43:11 +0100

    Update Verwaltung.java

--- src/Verwaltung.java
+++ src/Verwaltung.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Verwaltung {
     private Scanner scanner;
     private Caesar caesar;
+    private Viginere viginere;
 
     public static void main(String[] args) {
         new Verwaltung();
@@ -16,6 +17,9 @@ public class Verwaltung {
             System.out.println("== HAUPTMENÜ ==");
             System.out.println("[1] verschluesseln");
             System.out.println("[2] entschluesseln");
+            System.out.println("[3] viginere verschluesseln");
+            System.out.println("[4] viginere entschluesseln");
+
             int option = scanner.nextInt();
             scanner.nextLine();
             if (option == 1) {
@@ -40,6 +44,29 @@ public class Verwaltung {
                 this.caesar.setS(schluessel);
                 this.caesar.entschluesseln();
                 System.out.println(this.caesar.getKt());
+            }
+            if (option == 3) {
+                System.out.println("Gib das Wort an, welches Sie viginere verschluesseln wollen:");
+                String zeichenkette = scanner.nextLine();
+                System.out.println("Gib den Schlüssel an:");
+                String schluesselString = scanner.nextLine();
+                String schluessel = (schluesselString);
+                viginere = new Viginere();
+                this.viginere.setKt(zeichenkette);
+                this.viginere.setS(schluessel);
+                this.viginere.verschluesseln();
+                System.out.println(this.viginere.getGt());
+            }else if (option == 4) {
+                System.out.println("Gib das Wort an, welches Sie viginere entschluesseln wollen:");
+                String zeichenkette = scanner.nextLine();
+                System.out.println("Gib den Schlüssel an:");
+                String schluesselString = scanner.nextLine();
+                String schluessel = (schluesselString);
+                viginere = new Viginere();
+                this.viginere.setGt(zeichenkette);
+                this.viginere.setS(schluessel);
+                this.viginere.entschluesseln();
+                System.out.println(this.viginere.getKt());
             }
         }
     }
