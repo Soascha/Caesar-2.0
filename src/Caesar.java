977b6fc6de1d62de69e09bf3c8c41c336dbb8415
kosychsa <kosych.sascha@gmail.com>
2024-11-26 12:51:23 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Verwaltung.java

--- src/Caesar.java
+++ src/Caesar.java
@@ -1,58 +1,59 @@
 package src;
 
-public class Caesar {
-    private String Kt;
-    private int S;
-    private String Gt;
-    public Caesar(){
-        Gt ="";
-        Kt ="";
-    }
-    public void verschluesseln(){
-        Gt = "";
-        for (int i = 0 ; i < Kt.length() ; i++) {
-            int h = this.getASCII(Kt.charAt(i));
-            Gt = Gt + this.getChar(h + S);
+public class Caesar extends Kryptomat  {
+
+        private String Kt;
+        private int S;
+        private String Gt;
+        public Caesar(){
+            Gt ="";
+            Kt ="";
+        }
+        public void verschluesseln(){
+            Gt = "";
+            for (int i = 0 ; i < Kt.length() ; i++) {
+                int h = this.getASCII(Kt.charAt(i));
+                Gt = Gt + this.getChar(h + S);
+            }
+
+        }
+        public void entschluesseln(){
+            Kt = "";
+            for (int i = 0; i < Gt.length() ; i++) {
+                int k = this.getASCII(Gt.charAt(i));
+                Kt = Kt + this.getChar(k - S);
+            }
+
+
+        }
+        public int getASCII (char pWert) {
+            return  pWert;
+        }
+        public char getChar(int pWert) {
+            return (char) pWert;
         }
 
-    }
-    public void entschluesseln(){
-        Kt = "";
-        for (int i = 0; i < Gt.length() ; i++) {
-            int k = this.getASCII(Gt.charAt(i));
-            Kt = Kt + this.getChar(k - S);
+        public void setKt(String kt) {
+            Kt = kt;
         }
 
+        public void setS(int s) {
+            S = s;
+        }
 
-    }
-    private int getASCII (char pWert) {
-        return  pWert;
-    }
-    private char getChar(int pWert) {
-        return (char) pWert;
-    }
+        public void setGt(String gt) {
+            Gt = gt;
+        }
 
-    public void setKt(String kt) {
-        Kt = kt;
-    }
+        public int getS() {
+            return S;
+        }
 
-    public void setS(int s) {
-        S = s;
-    }
+        public String getGt() {
+            return Gt;
+        }
 
-    public void setGt(String gt) {
-        Gt = gt;
+        public String getKt() {
+            return Kt;
+        }
     }
-
-    public int getS() {
-        return S;
-    }
-
-    public String getGt() {
-        return Gt;
-    }
-
-    public String getKt() {
-        return Kt;
-    }
-}
--- src/Viginere.java
+++ src/Viginere.java
@@ -1,53 +1,61 @@
 package src;
 
-public class Viginere {
-    private String Kt;
-    private String S;
-    private String Gt;
+public class Viginere extends Kryptomat  {
+        private String Kt;
 
-    public Viginere() {
-        Gt = "";
-        Kt = "";
+        private String S;
+
+        private String Gt;
+
+        public Viginere() {
+            Gt = "";
+            Kt = "";
+            S = "";
+        }
+
+        public void verschluesseln() {
+            for ( int i = 0 ; i < Kt.length() ; i++) {
+                int j = this.getASCII(Kt.charAt(i));
+                int h = this.getASCII(S.charAt(i));
+                Gt = Gt + this.getChar(j + h);
+            }
+        }
+
+        public void entschluesseln() {
+            for (int i = 0; i < Gt.length(); i++) {
+                int n = this.getASCII(Gt.charAt(i));
+                int m = this.getASCII(S.charAt(i));
+                Kt = Kt + this.getChar(n - m);
+            }
+        }
+        public int getASCII (char pWert) {
+            return  pWert;
+        }
+        public char getChar(int pWert) {
+            return (char) pWert;
+        }
+
+        public void setGt(String pGt) {
+            Gt = pGt;
+        }
+
+        public String getGt() {
+            return Gt;
+        }
+        public void setKt (String pKt){
+            Kt = pKt;
+        }
+
+        public String getKt() {
+            return Kt;
+        }
+
+        public void setS (String pS) {
+            S =  pS;
+        }
+
+        public String getS () {
+            return S;
+        }
     }
 
-    public void verschluesseln() {
-
-    }
-
-    public void entschluesseln() {
-
-    }
-
-    private char zahlenZuBuchstaben(int pWert) {
-        return (char) pWert ;
-    }
-
-    private int buchstabenZuZahlen(char pWert) {
-        return (int) pWert ;
-    }
-
-    public void setGt(String pGt) {
-        Gt = pGt;
-    }
-
-    public String getGt() {
-        return Gt;
-    }
-    public void setKt (String pKt){
-        Kt = pKt;
-    }
-
-    public String getKt() {
-        return Kt;
-    }
-
-    public void setS (String S){
-        S = s;
-    }
-
-    public int getS () {
-        return S;
-    }
-
-
-}
